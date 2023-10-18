14d3addd99ef2909aa8dfd5b0df9dc2fa161b9bf
CCEO Developer <sara.garcia@cceo.com.mx>
2023-10-18 17:06:58 -0600

    :zap:

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Dashboard\TestController;
 use Illuminate\Support\Facades\Route;
+
+//use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    
-    return view('welcome');
-});
+Route::get('/',[ TestController::class,'index'] );
